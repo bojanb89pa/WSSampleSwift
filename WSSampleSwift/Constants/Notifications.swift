fbb5192d42f6c5b09b06ac09b9a3f2ef006723ee
bojanb-gecko <bojan.bogojevic@gecko.rs>
2017-02-03 09:46:12 +0100

    Reorganize files

---
+++ WSSampleSwift/Constants/Notifications.swift
@@ -0,0 +1,13 @@
+//
+//  Notifications.swift
+//  WSSample
+//
+//  Created by Bojan Bogojevic on 1/30/17.
+//  Copyright © 2017 Gecko Solutions. All rights reserved.
+//
+
+import Foundation
+
+
+public let NOTIFICATION_INTERNET_CONNECTION_NOT_AVAILABLE : String = "NOTIFICATION_INTERNET_CONNECTION_NOT_AVAILABLE"
+public let NOTIFICATION_INTERNET_CONNECTION_AVAILABLE = "NOTIFICATION_INTERNET_CONNECTION_AVAILABLE"
